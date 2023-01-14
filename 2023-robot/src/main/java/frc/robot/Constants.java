0b0227c944ee394a8286d31cd1aed7b1171a55cb
amycdeml <amycdeml@gmail.com>
2023-01-14 09:13:26 -0500

    joysticks

--- 2023-robot/src/main/java/frc/robot/Constants.java
+++ 2023-robot/src/main/java/frc/robot/Constants.java
@@ -5,11 +5,47 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final class Talon {
+        public static final int DRIVE_LEFT_TOP = 1; // falcons FX
+        public static final int DRIVE_LEFT_BOTTOM = 2; // falcons FX
+        public static final int DRIVE_RIGHT_TOP = 3; // falcons FX
+        public static final int DRIVE_RIGHT_BOTTOM = 4; // falcons FX
+
+        public static final int INTAKE_ROLLERS = 5; // Victor
+
+        public static final int MAGAZINE_ROLLERS = 6; // Victor
+        public static final int LOADER_ROLLER = 7; // Victor
+
+        public static final int ACCELERATOR_WHEEL_L = 8; // Talon SRX
+        public static final int ACCELERATOR_WHEEL_R = 9; // Talon SRX
+
+        public static final int SHOOTER_WHEEL_L = 10; // Falcon
+        public static final int SHOOTER_WHEEL_R = 11; // Falcon
+
+        public static final int CLIMBER_L = 12; // Falcon
+        public static final int CLIMBER_R = 13; // Flacon
+
+        public static final int HOOD = 14; // Talon SRX
+        public static final int INTAKE_ROLLERS_2 = 15; // Talon SRX
+
+    }
+
+    public final class PDP {
+        public static final int DRIVE_LEFT_FRONT = 1;
+        public static final int DRIVE_LEFT_REAR = 1;
+        public static final int DRIVE_RIGHT_FRONT = 1;
+        public static final int DRIVE_RIGHT_REAR = 1;
+    }
+}
--- 2023-robot/src/main/java/frc/robot/commands/UpdateSDLimelight.java
+++ 2023-robot/src/main/java/frc/robot/commands/UpdateSDLimelight.java
@@ -1,6 +1,6 @@
 package frc.robot.commands;
 
-import java.net.http.HttpResponse.PushPromiseHandler;
+// import java.net.http.HttpResponse.PushPromiseHandler;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
