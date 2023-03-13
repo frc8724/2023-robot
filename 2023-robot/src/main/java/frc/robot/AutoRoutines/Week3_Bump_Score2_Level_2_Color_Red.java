6e72de15985718e711bc8c43649c733002ee94f8
amycdeml <amycdeml@gmail.com>
2023-03-13 19:22:42 -0400

    bump auto

---
+++ 2023-robot/src/main/java/frc/robot/AutoRoutines/Week3_Bump_Score2_Level_2_Color_Red.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.AutoRoutines;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Week3_Bump_Score2_Level_2_Color_Red extends SequentialCommandGroup {
+  /** Creates a new Week3_Bump_Score2_Level_2_Color_Red. */
+  public Week3_Bump_Score2_Level_2_Color_Red() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(new Week3_Bump_Score2_Level_X_Color_X(2, 1));
+  }
+}
--- 2023-robot/src/main/java/frc/robot/RobotContainer.java
+++ 2023-robot/src/main/java/frc/robot/RobotContainer.java
@@ -56,6 +56,8 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    addAuto(new Week3_Bump_Score2_Level_2_Color_Red());
+
     addAuto(new Week3_Place2_Level_2_Color_Red());
     addAuto(new Week3_Place2_Level_3_Color_Red());
     addAuto(new Week3_Place2_Level_2_Color_Blue());
