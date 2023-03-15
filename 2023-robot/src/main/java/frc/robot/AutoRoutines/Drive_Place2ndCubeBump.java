7fd299e01a0917b7e8d99b53f61200f01e05b855
robertdeml <rdeml31@gmail.com>
2023-03-15 18:46:45 -0400

    added place cube

--- 2023-robot/src/main/java/frc/robot/AutoRoutines/Drive_Place2ndCubeBump.java
+++ 2023-robot/src/main/java/frc/robot/AutoRoutines/Drive_Place2ndCubeBump.java
@@ -5,13 +5,16 @@
 package frc.robot.AutoRoutines;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.ClawPistonSet;
 import frc.robot.commands.ClawRollerSet;
 import frc.robot.commands.DriveStraightOnHeading;
 import frc.robot.commands.ShoulderGoto;
 import frc.robot.commands.ShoulderWaitForPosition;
 import frc.robot.commands.SystemPlaceCone;
+import frc.robot.commands.SystemPlaceCube;
 import frc.robot.subsystems.ClawPiston.State;
 import frc.robot.subsystems.Shoulder;
 
@@ -31,9 +34,7 @@ public class Drive_Place2ndCubeBump extends SequentialCommandGroup {
     addCommands(
         new ParallelCommandGroup(
             new SequentialCommandGroup(
-                new SystemPlaceCone(Level),
-                new ShoulderGoto(Shoulder.LEVEL_X_SCORE[Level]),
-                new ShoulderWaitForPosition(),
+                new SystemPlaceCube(Level),
 
                 new ClawRollerSet(-.5),
                 new ClawPistonSet(State.CLOSE)),
--- 2023-robot/src/main/java/frc/robot/commands/SystemPlaceCube.java
+++ 2023-robot/src/main/java/frc/robot/commands/SystemPlaceCube.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Shoulder;
 
@@ -16,23 +15,10 @@ public class SystemPlaceCube extends SequentialCommandGroup {
   /** Creates a new SystemPlaceCube. */
   public SystemPlaceCube(int level) {
     // rotate shoulder to the location
-    addCommands(new ShoulderGoto(Shoulder.LEVEL_X_PRESCORE[level]));
+    addCommands(new ShoulderGoto(Shoulder.LEVEL_X_SCORE[level]));
     addCommands(new ShoulderWaitForPosition());
     // extend the arm out
     addCommands(new ArmSystemGoTo(Arm.LEVEL_X_SCORE[level]));
     addCommands(new ArmWaitForPosition());
-    // rotate shoulder down to place
-    addCommands(new ShoulderGoto(Shoulder.LEVEL_X_SCORE[level]));
-    addCommands(new ShoulderWaitForPosition());
-    // open the claw
-    addCommands(new ClawRollerSet(-0.1));
-    addCommands(new WaitCommand(0.2));
-    // retract the arm
-    addCommands(new ArmSystemGoTo(Arm.ALMOST_STOW));
-    addCommands(new ArmWaitForPosition());
-    addCommands(new ClawRollerSet(0.0));
-    // lower the shoulder
-    addCommands(new ShoulderGoto(Shoulder.STOW));
-    addCommands(new ShoulderWaitForPosition());
   }
 }
