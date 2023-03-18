9e98e177d5405ca873642939ac706c4185f98aa6
Driverstation Laptop <mayhem@mayhemrobotics.com>
2023-03-18 17:20:57 -0400

    going to match 47

--- 2023-robot/src/main/java/frc/robot/commands/DriveSystemOnChargingStation.java
+++ 2023-robot/src/main/java/frc/robot/commands/DriveSystemOnChargingStation.java
@@ -17,7 +17,8 @@ public class DriveSystemOnChargingStation extends SequentialCommandGroup {
     addCommands(new DriveStraightOnHeading(-0.05, -0.15, 20.0, 0));
     addCommands(new DriveStraightOnHeading(-0.15, 45.0, 0));
     addCommands(new WhackerSet(State.UP));
-    addCommands(new DriveStraightOnHeading(-0.15, -0.05, 20.0, 0));
+    addCommands(new DriveStraightOnHeading(-0.15, -0.1, 15.0, 0));
+    addCommands(new DriveStraightOnHeading(-0.1, -0.05, 7.0, 45));
 
     addCommands(new DriveBrakeMode(true));
     // addCommands(new DriveStraightOnHeading(-0.15, -0.05, 22.0, 0));
--- 2023-robot/src/main/java/frc/robot/commands/Drive_Place2ndCone.java
+++ 2023-robot/src/main/java/frc/robot/commands/Drive_Place2ndCone.java
@@ -6,8 +6,8 @@ public class Drive_Place2ndCone extends SequentialCommandGroup {
     public Drive_Place2ndCone(int color) {
         addCommands(new DriveStraightOnHeading(0.2, 20.0, -45.0 * color));
 
-        addCommands(new DriveStraightOnHeading(0.2, 0.5, 30.0, -5.0 * color));
-        addCommands(new DriveStraightOnHeading(0.5, 100.0, -5.0 * color));
+        addCommands(new DriveStraightOnHeading(0.2, 0.5, 30.0, 0.0 * color));
+        addCommands(new DriveStraightOnHeading(0.5, 100.0, 0.0 * color));
 
         addCommands(new DriveStraightOnHeading(0.5, 60.0, 0.0 * color));
 
